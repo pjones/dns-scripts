3283e2daf72447fefd0f987a1e87aae7262f975c
Peter Jones <pjones@devalot.com>
2020-08-04 17:10:29 -0700

    Rewrite the dyndns script to use the DNS Made Easy API

--- bin/update-dyndns.sh
+++ bin/update-dyndns.sh
@@ -1,87 +1,218 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 ################################################################################
-set -e
-set -u
+set -eu
+set -o pipefail
 
 ################################################################################
 export PATH=@extra_path@:$PATH
 
 ################################################################################
-cache=/tmp/dyndns-ip-cache
-url="https://cp.dnsmadeeasy.com/servlet/updateip?"
-domain_id=""
-domain_pass=""
-username=""
-ip=""
+cache_file=/tmp/dyndns-ip-cache
+base_url="https://api.dnsmadeeasy.com/V2.0"
+option_api_key_file=
+option_secret_key_file=
+option_domain_name=
+option_ip=
 
 ################################################################################
 usage() {
-cat <<EOF
-Usage: update-dyndns.sh [options]
+  cat <<EOF
+Usage: $(basename "$0") [options]
 
-  -d ID   Set domain ID
   -h      This message
-  -i ADDR Force DNS to use address ADDR
-  -p PASS Set domain password
-  -u NAME Set domain user name
+  -a FILE File Containing the API key
+  -d NAME The domain name to update
+  -i IP   Set the IP address manually
+  -s FILE File Containing the secret key
+  -S      Use the sandbox API instead of the production API
+
 EOF
 }
 
 ################################################################################
-while getopts "hd:p:u:i:" o; do
+make_request() {
+  local api_key=$1
+  shift
+
+  local secret_key=$1
+  shift
+
+  local http_date
+  local hmac
+
+  http_date=$(date -u +"%a, %d %b %Y %H:%M:%S GMT")
+  hmac=$(printf "%s" "$http_date" |
+    openssl dgst -hmac "$secret_key" -sha1 |
+    cut -d' ' -f2)
+
+  curl \
+    --verbose \
+    --location \
+    --header "Content-Type: application/json" \
+    --header "x-dnsme-apiKey: $api_key" \
+    --header "x-dnsme-requestDate: $http_date" \
+    --header "x-dnsme-hmac: $hmac" \
+    "$@"
+}
+
+################################################################################
+get_record_id() {
+  local name=$1
+
+}
+
+################################################################################
+get_record_by_name() {
+  local name=$1
+  jq --arg name "$name" '.data[]|select(.name == $name)'
+}
+
+################################################################################
+get_external_ip() {
+  if [ -n "$option_ip" ]; then
+    echo "$option_ip"
+  else
+    ip route get 1 | awk '{print $7;exit}'
+  fi
+}
+
+################################################################################
+set_domain_ip() {
+  local ip=$1
+
+  local api_key
+  local secret_key
+  local domain_name
+  local record_name
+  local domain_id
+  local record_obj
+  local record_id
+
+  api_key=$(cat "$option_api_key_file")
+  secret_key=$(cat "$option_secret_key_file")
+
+  domain_name=$(sed -E 's/^[^.]+\.//' <<<"$option_domain_name")
+  record_name=$(sed -E 's/^([^.]+).*$/\1/' <<<"$option_domain_name")
+
+  domain_id=$(
+    make_request \
+      "$api_key" \
+      "$secret_key" \
+      --request GET \
+      "$base_url/dns/managed/" |
+      jq --arg name "$domain_name" '.data[]|select(.name == $name)|.id'
+  )
+
+  if [ -z "$domain_id" ]; then
+    echo >&2 "ERROR: failed to lookup the domain ID for $domain_name"
+    exit 1
+  fi
+
+  record_obj=$(
+    make_request \
+      "$api_key" \
+      "$secret_key" \
+      "$base_url/dns/managed/$domain_id/records" |
+      jq --arg name "$record_name" '.data[]|select(.name == $name)'
+  )
+
+  if [ -z "$record_obj" ]; then
+    echo >&2 "ERROR: failed to lookup record ID for $record_name"
+    exit 1
+  fi
+
+  record_id=$(jq '.id' <<<"$record_obj")
+
+  if [ -z "$record_id" ]; then
+    echo >&2 "ERROR: failed to lookup record ID for $record_name"
+    exit 1
+  fi
+
+  record_obj=$(
+    jq --arg value "$ip" \
+      '.value = $value' <<<"$record_obj"
+  )
+
+  if [ -z "$record_obj" ]; then
+    echo >&2 "ERROR: failed to update the VALUE field"
+    exit 1
+  fi
+
+  make_request \
+    "$api_key" \
+    "$secret_key" \
+    --request PUT \
+    --data-raw "$record_obj" \
+    "$base_url/dns/managed/$domain_id/records/$record_id"
+}
+
+################################################################################
+main() {
+  local actual_ip
+  local cached_ip=""
+
+  if [ -z "$option_api_key_file" ] || [ ! -r "$option_api_key_file" ]; then
+    echo >&2 "ERROR: missing API key file"
+    exit 1
+  fi
+
+  if [ -z "$option_secret_key_file" ] || [ ! -r "$option_secret_key_file" ]; then
+    echo >&2 "ERROR: missing secret key file"
+    exit 1
+  fi
+
+  if [ -z "$option_domain_name" ]; then
+    echo >&2 "ERROR: missing domain name"
+    exit 1
+  fi
+
+  actual_ip=$(get_external_ip)
+
+  if [ -r "$cache_file" ]; then
+    cached_ip=$(head -1 "$cache_file")
+  fi
+
+  if [ "$actual_ip" != "$cached_ip" ]; then
+    echo "=> Updating IP for $option_domain_name to $actual_ip"
+    set_domain_ip "$actual_ip"
+    echo "$actual_ip" >"$cache_file"
+  fi
+}
+
+################################################################################
+while getopts "ha:d:i:s:S" o; do
   case "${o}" in
-    d) domain_id="$OPTARG"
-       ;;
+  h)
+    usage
+    exit
+    ;;
 
-    h) usage
-       exit
-       ;;
+  a)
+    option_api_key_file=$OPTARG
+    ;;
 
-    i) ip="$OPTARG"
-       ;;
+  d)
+    option_domain_name=$OPTARG
+    ;;
 
-    p) domain_pass="$OPTARG"
-       ;;
+  i)
+    option_ip=$OPTARG
+    ;;
 
-    u) username="$OPTARG"
-       ;;
+  s)
+    option_secret_key_file=$OPTARG
+    ;;
 
-    *) exit 1
-       ;;
+  S)
+    base_url="https://api.sandbox.dnsmadeeasy.com/V2.0"
+    ;;
+
+  *)
+    exit 1
+    ;;
   esac
 done
 
-shift $((OPTIND-1))
-
-################################################################################
-set -x
-
-################################################################################
-if [ -z "$ip" ]; then
-  ip=$(ip route get 1 | awk '{print $7;exit}')
-  #router=$(netstat -nr|awk '/^0.0.0.0/ {print $2}')
-  #ip=$(snmpwalk -Os -c public -v 1 "$router" ipAdEntAddr|grep -v -F 127.0.0.1|grep -v -F "$router" | cut -d' ' -f4)
-fi
-
-################################################################################
-if [ -r "$cache" ]; then
-  current_ip=$(cat "$cache")
-else
-  current_ip=""
-fi
-
-################################################################################
-if [ "$current_ip" != "$ip" ]; then
-  url="${url}username=${username}&password=${domain_pass}&"
-  url="${url}id=${domain_id}&ip=${ip}"
-
-  if curl -s "$url" | grep -q success; then
-    echo "$ip" > "$cache"
-  else
-    echo "Failed to update dynamic IP address!"
-    exit 1
-  fi
-else
-  echo "IP address hasn't changed (matches cache)"
-fi
+shift $((OPTIND - 1))
+main
